fd021caabd3ce83b57a07877164cefafb90947d6
amer236 <amer236@aucklanduni.ac.nz>
2014-10-10 13:53:44 +1300

    Added sliders to theme changer

--- src/sidePanel/ThemeSelector.java
+++ src/sidePanel/ThemeSelector.java
@@ -9,6 +9,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
 import javax.swing.UIManager;
 
 import net.miginfocom.swing.MigLayout;
@@ -18,56 +19,57 @@ import net.miginfocom.swing.MigLayout;
  * Taken from SE206 Assignment 3, paired prototype.
  */
 public class ThemeSelector extends JFrame {
-
+	JSlider red = new JSlider(0,255);
+	JSlider green = new JSlider(0,255);
+	JSlider blue = new JSlider(0,255);
+	
 	public ThemeSelector(final JFrame frame) {
+		this.setTitle("Customise Theme");
 		JPanel panel = new JPanel(new MigLayout());
+
+		panel.add(new JLabel("Red"), "grow");
+		panel.add(red, "wrap, grow");
+		panel.add(new JLabel("Green"), "grow");
+		panel.add(green, "wrap, grow");
+		panel.add(new JLabel("Blue"), "grow");
+		panel.add(blue, "wrap, grow");
 		
-		panel.add(new JLabel("NimbusBase"));
-		panel.add(new JLabel("Control"), "wrap");
+		//panel.add(new JLabel("NimbusBase"));
+		//panel.add(new JLabel("Control"), "wrap");
 		
-		JButton greenBase = new JButton("Green");
-		greenBase.addActionListener(new ActionListener() {
+		JButton base = new JButton("Set Base Colour");
+		base.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				UIManager.put("nimbusBase", Color.green);
+				UIManager.put("nimbusBase", new Color(red.getValue(), blue.getValue(), green.getValue()));
 				frame.repaint();
 				repaint();
 			}
 		});
-		panel.add(greenBase,"grow");
+		panel.add(base,"grow");
 		
-		JButton greenControl = new JButton("Green");
-		greenControl.addActionListener(new ActionListener() {
+		JButton control = new JButton("Set Control Color");
+		control.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				UIManager.put("control", Color.green);
+				UIManager.put("control", new Color(red.getValue(), blue.getValue(), green.getValue()));
 				frame.repaint();
 				repaint();
 			}
 		});
-		panel.add(greenControl, "wrap, grow");
+		panel.add(control, "grow, wrap");
 		
-		JButton redBase = new JButton("Red");
-		redBase.addActionListener(new ActionListener() {
+		JButton highlight = new JButton("Set Highlight Color");
+		control.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				UIManager.put("nimbusBase", Color.red);
+				UIManager.put("nimbusBlueGrey", new Color(red.getValue(), blue.getValue(), green.getValue()));
+				UIManager.put("nimbusFocus", new Color(red.getValue(), blue.getValue(), green.getValue()));
 				frame.repaint();
 				repaint();
 			}
 		});
-		panel.add(redBase,"grow");
-		
-		JButton redControl = new JButton("Red");
-		redControl.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				UIManager.put("control", Color.red);
-				frame.repaint();
-				repaint();
-			}
-		});
-		panel.add(redControl,"wrap, grow");
+		panel.add(highlight, "grow, wrap, span");
 		
 		JButton def = new JButton("Default All");
 		def.addActionListener(new ActionListener() {
@@ -75,11 +77,13 @@ public class ThemeSelector extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				UIManager.put("control", new Color(214, 217, 223));
 				UIManager.put("nimbusBase", new Color(51,98,140));
+				UIManager.put("nimbusBlueGrey", new Color(169,176,190));
+				UIManager.put("nimbusFocus", new Color(115,164,209));
 				repaint();
 				frame.repaint();
 			}
 		});
-		panel.add(def,"span, grow");
+		panel.add(def,"span, grow, wrap");
 		
 		JButton done = new JButton("Done");
 		done.addActionListener(new ActionListener() {
@@ -91,7 +95,9 @@ public class ThemeSelector extends JFrame {
 			}
 		});
 		panel.add(done,"span, grow");
-
+		
+		
+		
 		this.setPreferredSize(new Dimension(300, 300));
 		this.pack();
 		this.add(panel);
