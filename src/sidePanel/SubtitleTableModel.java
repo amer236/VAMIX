6f58940bc215c0dd6f013b47bf9043ae52585589
amer236 <amer236@aucklanduni.ac.nz>
2014-10-22 20:38:31 +1300

    Subtitling functionality complete, checks still required

--- src/sidePanel/SubtitleTableModel.java
+++ src/sidePanel/SubtitleTableModel.java
@@ -2,30 +2,23 @@ package sidePanel;
 
 import java.util.ArrayList;
 
-import javax.swing.event.TableModelListener;
-import javax.swing.table.DefaultTableModel;
+import javax.swing.table.AbstractTableModel;
 
-public class SubtitleTableModel extends DefaultTableModel {
+public class SubtitleTableModel extends AbstractTableModel {
 	
 	private String[] colNames = new String[3];
-	private ArrayList<int[]> dataEntries = new ArrayList<int[]>();
+	private ArrayList<String[]> dataEntries = new ArrayList<String[]>();
 	
 	public SubtitleTableModel(){
 		colNames[0] = "Start Time";
 		colNames[1] = "Stop Time";
 		colNames[2] = "Text";
 	}
-	
-	public void addDataRow(){
-		int[] obj = new int[3];
-		dataEntries.add(obj);
-	}
 
-	@Override
-	public Class<?> getColumnClass(int columnIndex) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+//	public Class getColumnClass(int c) {
+//        return getValueAt(0, c).getClass();
+//    }
+
 
 	@Override
 	public int getColumnCount() {
@@ -39,18 +32,35 @@ public class SubtitleTableModel extends DefaultTableModel {
 
 	@Override
 	public int getRowCount() {
+		//return 10;
 		return dataEntries.size();
 	}
 
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
+		//return "poop";
 		return dataEntries.get(rowIndex)[columnIndex];
 	}
 
 
 	@Override
 	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-		dataEntries.get(rowIndex)[rowIndex] = (int) aValue;
+		dataEntries.get(rowIndex)[rowIndex] = (String) aValue;
 
+	}
+	
+	public void addDataRow(String s, String t, String text){
+		String[] obj = new String[3];
+		obj[0] = s;
+		obj[1] = t;
+		obj[2] = text;
+		
+		dataEntries.add(obj);
+		fireTableDataChanged();
+	}
+	
+	public void deleteDataRow(int row){		
+		dataEntries.remove(row);
+		fireTableDataChanged();
 	}
 }
--- src/sidePanel/SubtitlesPanel.java
+++ src/sidePanel/SubtitlesPanel.java
@@ -4,6 +4,9 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -11,9 +14,13 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.Timer;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 import javax.swing.table.TableModel;
 
 import operations.BashOperations;
@@ -31,35 +38,124 @@ public class SubtitlesPanel extends SidePanel {
 	
 	JButton _saveVideo;
 	
-	TableModel subtitleData = new SubtitleTableModel();
-	JTable subsTable = new JTable(subtitleData);
+	SubtitleTableModel subtitleData;
+	JTable subsTable;
 	
 	JButton addButton = new JButton("Add Row");
+	JButton deleteButton = new JButton("Delete Selected Row");
+	
+	JLabel hours = new JLabel("Hours");
+	JLabel mins = new JLabel("Minutes");
+	JLabel secs = new JLabel("Seconds");
+	
+	JLabel start = new JLabel("Start Time");
+	JLabel finish = new JLabel("End Time");
+	
+	JSpinner startHour = new JSpinner();
+	JSpinner startMin = new JSpinner();
+	JSpinner startSec = new JSpinner();
+	
+	JSpinner endHour = new JSpinner();
+	JSpinner endMin = new JSpinner();
+	JSpinner endSec = new JSpinner();
+	
+	JLabel text = new JLabel("Text: ");
+	JTextField subtitle = new JTextField();
+	
+	JButton save = new JButton("Save Subtitle File");
+
 	
 	boolean isUsable = true;
 	String _outFile;
 
 	public SubtitlesPanel(String name, GeneralPanel generalPanel) {
 		super(name);
-
 		super.setupPanel();
 
 		_generalPanel = generalPanel;
-		
-
-		this.add(subsTable);
 		}
 
 	protected void setupPanel() {
-		this.add(addButton);
+		subtitleData = new SubtitleTableModel();
+		subsTable = new JTable(subtitleData);
+		subsTable.setPreferredScrollableViewportSize(new Dimension(500, 70));
+		subsTable.setFillsViewportHeight(true);
+		
+		JScrollPane scrollPane = new JScrollPane(subsTable);
+        
+		this.add(scrollPane, "grow, wrap, span");
+
+		this.add(deleteButton, "span, wrap, grow");
+		this.add(new JLabel(""));
+		this.add(hours, "grow");
+		this.add(mins, "grow");
+		this.add(secs, "wrap, grow");
+		this.add(start, "grow");
+		this.add(startHour, "grow");
+		this.add(startMin, "grow");
+		this.add(startSec, "grow");
+		this.add(new JLabel(""), "wrap");
+		this.add(finish, "grow");
+		this.add(endHour, "grow");
+		this.add(endMin, "grow");
+		this.add(endSec, "grow");
+		this.add(new JLabel(""), "wrap");
+		this.add(text, "grow");
+		this.add(subtitle, "grow, wrap, span");
+
+		this.add(addButton, "span, grow, wrap");
+		this.add(save, "grow, span");
+		
 		addButton.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				subsTable.getModel().
-				
+				subtitleData.addDataRow(startHour.getValue().toString() + ":" + startMin.getValue().toString() + ":" + startSec.getValue().toString(),
+						endHour.getValue().toString() + ":" + endMin.getValue().toString() + ":" + endSec.getValue().toString(),
+						subtitle.getText());
 			}
-			
 		});
+		
+		deleteButton.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(subsTable.getSelectedRow() != -1){
+					subtitleData.deleteDataRow(subsTable.getSelectedRow());
+				}
+			}
+		});
+		
+		save.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				saveSRTFile();
+			}
+		});
+	}
+
+	public void saveSRTFile() {
+		String outtext;
+		File inputMedia = new File(_generalPanel.getInputField());
+		int location = inputMedia.getAbsolutePath().lastIndexOf(".");
+		if(location == -1){
+			outtext = inputMedia.getName();
+		}else{
+			outtext = _generalPanel.getInputField().substring(0, location);
+		}
+		try {
+			File outSRT = new File(outtext + ".srt");
+			outSRT.createNewFile();
+			PrintWriter output = new PrintWriter(outtext + ".srt");
+			System.out.println(outtext);
+			for(int i = 0; i < subtitleData.getRowCount(); i++){
+				output.println();
+				output.println(i);
+				output.println(subtitleData.getValueAt(i, 0) + ",000 --> " + subtitleData.getValueAt(i, 1) + ",000");
+				output.println(subtitleData.getValueAt(i, 2));
+			}
+			output.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 	}
 
 }
